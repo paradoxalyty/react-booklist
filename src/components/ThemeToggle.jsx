d2fb6a1a78bc5239e10cbebd293898c947d09fa5
paradoxalyty <paradoxalyty@gmail.com>
2022-12-15 10:12:06 +0100

    add themeToggler

---
+++ src/components/ThemeToggle.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
+
+class ThemeToggle extends Component {
+    static contextType = ThemeContext;
+
+    render() {
+        const { toggleTheme } = this.context;
+        return <button onClick={toggleTheme}>Toggle The Theme</button>;
+    }
+}
+
+export default ThemeToggle;
