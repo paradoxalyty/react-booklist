5b3be938e193ecc0f2296b6f5222d5b657d2b813
paradoxalyty <paradoxalyty@gmail.com>
2022-12-15 13:00:05 +0100

    Changeing BookList, Navbar and ThemeToggle class components to the functional components with using useContext hook.

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,13 +1,9 @@
-import { Component } from "react";
+import { useContext } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
 
-class ThemeToggle extends Component {
-    static contextType = ThemeContext;
-
-    render() {
-        const { toggleTheme } = this.context;
-        return <button onClick={toggleTheme}>Toggle The Theme</button>;
-    }
-}
+const ThemeToggle = () => {
+    const { toggleTheme } = useContext(ThemeContext);
+    return <button onClick={toggleTheme}>Toggle The Theme</button>;
+};
 
 export default ThemeToggle;
