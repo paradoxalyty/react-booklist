87d784760b2cd9a7891b2d44d69a8f112cf15f61
paradoxalyty <paradoxalyty@gmail.com>
2022-12-16 12:35:13 +0100

    add NewBookForm component with css

---
+++ src/components/NewBookForm.js
@@ -0,0 +1,40 @@
+import { useContext, useState } from "react";
+import { BookContext } from "../contexts/BookContext";
+
+const NewBookForm = () => {
+    const { addBook } = useContext(BookContext);
+    const [title, setTitle] = useState("");
+    const [author, setAuthor] = useState("");
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        addBook(title, author);
+        setTitle("");
+        setAuthor("");
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                placeholder="book title"
+                value={title}
+                required
+                onChange={(e) => {
+                    setTitle(e.target.value);
+                }}
+            />
+            <input
+                type="text"
+                placeholder="author"
+                value={author}
+                required
+                onChange={(e) => {
+                    setAuthor(e.target.value);
+                }}
+            />
+            <input type="submit" value="add book" />
+        </form>
+    );
+};
+
+export default NewBookForm;
